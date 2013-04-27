02ed0f057e7fb26c2a75cd5d774fb1210acc009b
Kay Robbins <krobbins@cs.utsa.edu>
2013-04-27 10:15:54 -0500

    fixed renaming of geteegtags.m to findtags.m

--- ctagger/tests/test_findtags.m
+++ ctagger/tests/test_findtags.m
@@ -1,4 +1,4 @@
-function test_suite = test_geteegtags%#ok<STOUT>
+function test_suite = test_findtags%#ok<STOUT>
 initTestSuite;
 
 function values = setup %#ok<DEFNU>
@@ -28,39 +28,39 @@ function teardown(values) %#ok<INUSD,DEFNU>
 % Function executed after each test
 
 function testValidValues(values)  %#ok<DEFNU>
-% Unit test for geteegtags
-fprintf('\nUnit tests for geteegtags\n');
+% Unit test for findtags
+fprintf('\nUnit tests for findtags\n');
 fprintf('It should tag an EEG structure that hasn''t been tagged\n');
 assertTrue(~isfield(values.EEGEpoch.etc, 'eventHedTags'));
-eTags = geteegtags(values.EEGEpoch);
+eTags = findtags(values.EEGEpoch);
 events = eTags.getEvents();
 assertEqual(length(events), 2);
 assertTrue(~isempty(eTags.getHedXML()));
 fprintf('It should work if EEG doesn''t have .etc field\n');
 EEG1 = values.EEGEpoch;
 EEG1 = rmfield(EEG1, 'etc');
-eTags1 = geteegtags(EEG1);
+eTags1 = findtags(EEG1);
 events1 = eTags1.getEvents();
 assertEqual(length(events1), 2);
 assertTrue(~isempty(eTags1.getHedXML()));
 fprintf('It should work if EEG has an empty .etc field\n');
 EEG2 = values.EEGEpoch;
 EEG2.etc = '';
-eTags2 = geteegtags(EEG2);
+eTags2 = findtags(EEG2);
 events2 = eTags2.getEvents();
 assertEqual(length(events2), 2);
 assertTrue(~isempty(eTags2.getHedXML()));
 fprintf('It should work if EEG has a non-structure .etc field\n');
 EEG3 = values.EEGEpoch;
 EEG3.etc = 'This is a test';
-eTags3 = geteegtags(EEG3);
+eTags3 = findtags(EEG3);
 events3 = eTags3.getEvents();
 assertEqual(length(events3), 2);
 assertTrue(~isempty(eTags3.getHedXML()));
 fprintf('It should work if the EEG has already been tagged\n');
 json1 = eTags1.getJson();
 EEG3.etc.eventHedTags = json1;
-eTags4 = geteegtags(EEG3);
+eTags4 = findtags(EEG3);
 events4 = eTags4.getEvents();
 assertEqual(length(events4), 2);
 assertTrue(~isempty(eTags4.getHedXML()));
